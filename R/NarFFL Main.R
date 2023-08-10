1204603eccd50f6afece7760c35e7e8455536d55
Randal Morris <morrrand@gmail.com>
2023-08-10 06:40:19 -0500

    Add 2022 NarFFL Data

--- R/NarFFL Main.R
+++ R/NarFFL Main.R
@@ -2,6 +2,7 @@
 
 #Load Packages
 #remotes::install_github("ffverse/ffscrapr")
+#remotes::install_github("RandalMorris/ffscrapr", ref = "Flea_Add")
 
 #Load Functions and Data
 # source("functions.R")
@@ -17,7 +18,8 @@ player_scores = ffscrapr::ff_playerscores(
   ffscrapr::fleaflicker_connect(season = Fantasy_Year, league_id = "140956"), page_limit = 25)
 
 #Get Fresh data
-for (x in 1:3){
+start_time <- Sys.time()
+for (x in 1:nrow(NarFFL_leagues)){
   conn <- ffscrapr::fleaflicker_connect(season = Fantasy_Year, league_id = NarFFL_leagues$id[x])
   
   cat(paste0("Working League ", NarFFL_leagues$name[x]))
@@ -35,9 +37,11 @@ for (x in 1:3){
   starters = rbind(starters, starters_tmp)
   transactions = rbind(transactions, transactions_tmp)
   
-  rm(franchises_tmp, rosters_tmp, standings_tmp, starters_tmp, transactions_tmp, conn)
+  rm(franchises_tmp, rosters_tmp,standings_tmp,  starters_tmp, transactions_tmp, conn)
 }
-rm(x)
+end_time <- Sys.time()
+Duration = paste0("Execution Time: ", round(end_time - start_time,2), " Minutes")
+rm(x, end_time, start_time)
 
 save.image(file = paste0(getwd(), "/data/NarFFL_Data_2022.rdata"))
 
